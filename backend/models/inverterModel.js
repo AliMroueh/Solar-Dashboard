47198af7b94085bc92066079029dad65a57b9d38
Yassertrb <yassertraboulsi@gmail.com>
2023-03-13 21:01:53 +0200

    my page edit

--- backend/models/inverterModel.js
+++ backend/models/inverterModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const inverterSchema = new mongoose.Schema({
     type: { type: String, required: true },
-    strength: { type: String, required: true },
+    strength: { type: Number, required: true },
     description: { type: String, required: true },
     inverterImage: { type: String },
 },
