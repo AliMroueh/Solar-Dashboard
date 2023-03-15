cf5bc1e7da7551c01b1b73da272e445462bb2fb3
Ali <mroueh_ali@outlook.com>
2023-03-15 13:41:43 +0200

    addbattery

--- frontend/src/screens/AdminSolarBatteries.tsx
+++ frontend/src/screens/AdminSolarBatteries.tsx
@@ -117,7 +117,9 @@ export default function AdminSolarBatteries(): JSX.Element {
       {loading ? (
         <div>loading...</div>
       ) : (
-        <table className='shadow-2x1 font-[Poppins] border-2 border-cyan-200 mt-4 w-full'>
+        <div className='overflow-x-auto'>
+        <table className='shadow-2x1 font-[Poppins] border-2 border-cyan-200 mt-4 w-full overflow-x-auto'>
+        {/* // <table className='shadow-2x1 font-[Poppins] border-2 border-cyan-200 w-11/12 mx-auto'> */}
           <thead className='text-white'>
             <tr>
               <th className='py-3 bg-cyan-800 text-center'>ID</th>
@@ -134,14 +136,14 @@ export default function AdminSolarBatteries(): JSX.Element {
                 <td className='py-3 px-6 text-center'>{row.type}</td>
                 <td className='py-3 px-6 text-center'>{row.capacity}</td>
                 <td className='py-3 px-6 text-center'>{row.description}</td>
-                <td className='py-3 px-6 text-center'><img src={row.batteryImage} alt="Battery" /> </td>
-                <td className='py-3 px-6 text-center'>
+                <td className='py-3 px-6 text-center xs:py-0 px-0'><img src={row.batteryImage} alt="Battery" className='w-20 xs:w-full' /> </td>
+                <td className='py-3 px-6 text-center flex xs:justify-between'>
                 <Link to={`/UpdateBatteryPanel/${row._id}?type=${row.type}&capacity=${row.capacity}&description=${row.description}&batteryImage=${row.batteryImage}`}>
-                    <button type='button' className='edit w-auto p-4 bg-blue-600 ml-8 text-slate-200 rounded-md self-end'>
+                    <button type='button' className='edit w-auto p-2 bg-blue-600 text-slate-200 rounded-md'>
                       Edit
                     </button>
                   </Link>
-                  <button type='button' className='delete w-auto p-4 bg-red-600 mr-4 text-slate-200 rounded-md self-end' onClick={(event) => deleteHandler(event, row._id)}>
+                  <button type='button' className='delete w-auto p-2 bg-red-600 text-slate-200 rounded-md ml-2' onClick={(event) => deleteHandler(event, row._id)}>
                     Delete
                   </button>
                 </td>
@@ -149,6 +151,7 @@ export default function AdminSolarBatteries(): JSX.Element {
             ))}
           </tbody>
         </table>
+        </div>
       )}
     </div>
   );
