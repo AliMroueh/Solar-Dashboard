30d6f5227dfd98bc766694ab90233fb8c0a62470
Ali <mroueh_ali@outlook.com>
2023-03-27 14:34:49 +0300

    update message box

--- frontend/src/components/MessageBox.tsx
+++ frontend/src/components/MessageBox.tsx
@@ -1,13 +1,74 @@
-import React from 'react'
+// import React from 'react'
+
+// type props={
+//     variant?: 'success' | 'danger' | 'info',
+//     children: React.ReactNode
+// }
+// export default function MessageBox(props: props) {
+//     return (
+//         <div className={`alert alert-${props.variant || 'info'}`}>
+//             {props.children}
+//         </div>
+//     )
+// }
+
+// import React, { useEffect, useState } from 'react';
+
+// type props={
+//     variant?: 'success' | 'danger' | 'info',
+//     children: React.ReactNode
+// }
+
+// export default function MessageBox(props: props) {
+//     const [isVisible, setIsVisible] = useState(true);
+
+//     useEffect(() => {
+//         const timer = setTimeout(() => {
+//             setIsVisible(false);
+//         }, 2000);
+
+//         return () => clearTimeout(timer);
+//     }, []);
+
+//     return (
+//         <>
+//             {isVisible && (
+//                 <div className={`alert alert-${props.variant || 'info'}`}>
+//                     {props.children}
+//                 </div>
+//             )}
+//         </>
+//     )
+// }
+
+import React, { useEffect, useState } from 'react';
 
 type props={
     variant?: 'success' | 'danger' | 'info',
     children: React.ReactNode
 }
+
 export default function MessageBox(props: props) {
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        if (props.variant === 'info') {
+            const timer = setTimeout(() => {
+                setIsVisible(false);
+            }, 2000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [props.variant]);
+
     return (
-        <div className={`alert alert-${props.variant || 'info'}`}>
-            {props.children}
-        </div>
+        <>
+            {isVisible && (
+                <div className={`alert alert-${props.variant || 'info'}`}>
+                    {props.children}
+                </div>
+            )}
+        </>
     )
 }
+
